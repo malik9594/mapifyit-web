4ed96de1c7550ff84f04762d364545d54f06a017
malik9594 <135107174+malik9594@users.noreply.github.com>
2026-03-25 11:56:17 +0500

    Changing in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -149,7 +149,7 @@ export default function Footer() {
                         {[
                             { name: "Privacy Policy", href: "/privacy-policy" },
                             { name: "Terms of Service", href: "/terms-of-service" },
-                            { name: "Cookie Policy", href: "#" }
+                            // { name: "Cookie Policy", href: "#" }
                         ].map((item) => (
                             <Link
                                 key={item.name}
